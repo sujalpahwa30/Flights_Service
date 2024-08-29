eb5c93bdd0f4263789eeadf41e49bbe1e070e593
Sujal Pahwa <sujalpahwa4@gmail.com>
2024-08-29 19:52:01 +0530

    Adding the code for initial setup with one CRUD Api done

---
+++ src/controllers/airplane-controller.js
@@ -0,0 +1,26 @@
+const { StatusCodes } = require('http-status-codes');
+
+const { AirplaneService } = require('../services');
+const { ErrorResponse , SuccessResponse } = require('../utils/common');
+
+async function createAirplane(req, res) {
+    try {
+        const airplane = await AirplaneService.createAirplane({
+            modelNumber: req.body.modelNumber,
+            capacity: req.body.capacity
+        });
+        SuccessResponse.data = airplane;
+        return res
+            .status(StatusCodes.CREATED)
+            .json(SuccessResponse);
+    } catch (error) {
+        ErrorResponse.error = error;
+        return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+    }
+}
+
+module.exports = {
+    createAirplane
+}
